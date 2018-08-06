93914f67a1d555f39de07441b6beeda6ee9f85ae
Jeremy Cliff Armstrong <JadedDragoon@users.noreply.github.com>
2018-08-06 14:32:19 -0500

    test commit 2

--- functions.php
+++ functions.php
@@ -5,7 +5,6 @@ function brotherhood_enqueue_styles() {
 	$parent_style = 'twentyseventeen-style';
 	$child_style  = 'brotherhood-style';
 
-
 	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( $child_style,
 		get_stylesheet_directory_uri() . '/style.css',
