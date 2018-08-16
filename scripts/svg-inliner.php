10a34819cd0c033b511fcb263678a298c5639e0a
Jeremy Cliff Armstrong <JadedDragoon@users.noreply.github.com>
2018-08-16 13:32:56 -0500

    impoved file checks, cleaned up phpcs exceptions

--- scripts/svg-inliner.php
+++ scripts/svg-inliner.php
@@ -1,8 +1,5 @@
 <?php
-/**
- * initialize sgv sanitizer
- * https://github.com/darylldoyle/svg-sanitizer
- */
+/* https://github.com/darylldoyle/svg-sanitizer */
 use enshrined\svgSanitize\Sanitizer;
 
 add_filter( 'the_content', 'svg_inliner' );
@@ -14,40 +11,34 @@ function svg_inliner( $content ) {
 
 	$post->loadHTML( $content );
 	$img_list = $post->getElementsByTagName( 'img' );
-	/**
-	 * Iteration time
-	 * (regressive loop because http://php.net/manual/en/domnode.replacechild.php#50500)
-	*/
+
+	/* regressive loop because http://php.net/manual/en/domnode.replacechild.php#50500 */
 	$i = $img_list->length - 1;
 	while ( $i > -1 ) {
 		$img     = $img_list->item( $i );
 		$src_url = parse_url( $img->getAttribute( 'src' ), PHP_URL_PATH );
 		$src_ext = pathinfo( $src_url, PATHINFO_EXTENSION );
-		if ( 'svg' !== $src_ext ) {
-			$i--;
-			continue;
-		}
+		if ( 'svg' !== $src_ext ) { $i--; continue; } /* phpcs:ignore Squiz.ControlStructures.ControlSignature.NewlineAfterOpenBrace, Generic.Formatting.DisallowMultipleStatements.SameLine */
 
+		// no x-site monkey business
 		$svg_host  = parse_url( $img->getAttribute( 'src' ), PHP_URL_HOST );
 		$this_host = parse_url( get_site_url(), PHP_URL_HOST );
-		if ( $this_host !== $svg_host ) {
-			$i--;
-			continue; // no x-site monkey business
-		}
-		$svg = new DOMDocument();
+		if ( $this_host !== $svg_host ) { $i--; continue; } /* phpcs:ignore Squiz.ControlStructures.ControlSignature.NewlineAfterOpenBrace, Generic.Formatting.DisallowMultipleStatements.SameLine */
+
+		$svg_local_path = WP_CONTENT_DIR . substr(
+			parse_url( $src_url, PHP_URL_PATH ),
+			strpos( parse_url( $src_url, PHP_URL_PATH ), 'wp-content/', 1 ) + 10
+		);
 
 		// load the SVG and parse it (and sanitize... obv)
-		$svg_local_path = WP_CONTENT_DIR . substr( parse_url( $src_url, PHP_URL_PATH ), strpos( parse_url( $src_url, PHP_URL_PATH ), 'wp-content/', 1 ) + 10 );
-		$clean_svg      = $sanitizer->sanitize( file_get_contents( $svg_local_path ) );
-		/* phpcs:ignore Generic.ControlStructures.InlineControlStructure.NotAllowed */
-		if ( ! $clean_svg ) {
-			$i--;
-			continue;
-		}
+		if ( ! file_exists( $svg_local_path ) ) { $i--; continue; } /* phpcs:ignore Squiz.ControlStructures.ControlSignature.NewlineAfterOpenBrace, Generic.Formatting.DisallowMultipleStatements.SameLine */
+		$clean_svg = $sanitizer->sanitize( file_get_contents( $svg_local_path ) );
+		if ( ! $clean_svg ) { $i--; continue; } /* phpcs:ignore Squiz.ControlStructures.ControlSignature.NewlineAfterOpenBrace, Generic.Formatting.DisallowMultipleStatements.SameLine */
+		$svg = new DOMDocument();
 		$svg->loadXML( $clean_svg );
 
-		// swap svg in for image
-		$img->parentNode->replaceChild(
+		// replace img with svg
+		$img->parentNode->replaceChild( /* phpcs:ignore WordPress.NamingConventions.ValidVariableName.NotSnakeCaseMemberVar */
 			$post->importNode(
 				$svg->getElementsByTagName( 'svg' )->item( 0 ),
 				true
